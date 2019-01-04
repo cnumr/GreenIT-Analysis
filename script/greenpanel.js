918584773b38838bdccdd402d04b54b7a34be462
didierfred <didierfred@gmail.com>
2019-01-04 15:58:04 +0100

    clean code

--- script/greenpanel.js
+++ script/greenpanel.js
@@ -30,79 +30,54 @@ function   (esprima) {
 
   function initPanel() {
     openBackgroundPageConnection();
-    document.getElementById('launchAnalyse').addEventListener('click',function (e) {launchAnalyse();});
-    //document.getElementById('viewHistory').addEventListener('click',function (e) {viewHistory();});	
-	
+    document.getElementById('launchAnalyse').addEventListener('click',function (e) {launchAnalyse();});		
   }
 
   function openBackgroundPageConnection() {
-    // Create a connection to the background page
     backgroundPageConnection = chrome.runtime.connect({
       name: "greenDevPanel-page"
     });
-
     backgroundPageConnection.onMessage.addListener(function (frameMeasures) {
       // Handle responses from the background page
-      //console.log("Analyse received = " + JSON.stringify(frameMeasures));
       aggregateFrameMeasures(frameMeasures);
       refreshUI();
     });
   }
 
   function aggregateFrameMeasures(frameMeasures) {
-  
-    debug(() => `receive from frameAnalyse.js ${JSON.stringify(frameMeasures)}`);
-
+    // debug(() => `receive from frameAnalyse.js ${JSON.stringify(frameMeasures)}`);
     measures.domSize += frameMeasures.domSize;
     measures.ecoIndex = calculEcoIndex(measures.domSize,measures.nbRequest,Math.round(measures.responsesSize/1000));
     measures.grade = getEcoIndexGrade(measures.ecoIndex);
- 
     measures.pluginsNumber += frameMeasures.pluginsNumber;
-    if (measures.pluginsNumber>0) {
-      ecoRules.get("plugins").status = "NOK";
-      ecoRules.get("plugins").comment = measures.pluginsNumber + " plugin(s) found";
-    }
+    if (measures.pluginsNumber>0) setRuleValues("plugins",false,measures.pluginsNumber + " plugin(s) found");
  
     if (measures.styleSheetsNumber < frameMeasures.styleSheetsNumber) {
       measures.styleSheetsNumber = frameMeasures.styleSheetsNumber;
-      if (measures.styleSheetsNumber>2) {
-        ecoRules.get("styleSheets").status = "NOK";
-        ecoRules.get("styleSheets").comment = measures.styleSheetsNumber + " stylesheets found for at least one frame";
-      }
+      if (measures.styleSheetsNumber>2) setRuleValues("styleSheets",false,measures.styleSheetsNumber + " stylesheets found for at least one frame");
     }
-
     measures.printStyleSheetsNumber += frameMeasures.printStyleSheetsNumber;
-    if (measures.printStyleSheetsNumber > 0 ) {
-      ecoRules.get("printStyleSheets").status = "OK";
-      ecoRules.get("printStyleSheets").comment = measures.printStyleSheetsNumber + " print StyleSheet(s) found";
-    }
+    if (measures.printStyleSheetsNumber > 0 ) setRuleValues("printStyleSheets",true,measures.printStyleSheetsNumber + " print StyleSheet(s) found");
 
     if (measures.inlineStyleSheetsNumber < frameMeasures.inlineStyleSheetsNumber) {
       measures.inlineStyleSheetsNumber = frameMeasures.inlineStyleSheetsNumber;
-      if (measures.inlineStyleSheetsNumber>0) {
-        ecoRules.get("externalizeCss").status = "NOK";
-        ecoRules.get("externalizeCss").comment = measures.inlineStyleSheetsNumber + " inline stylesheets found ";
-      }
+      if (measures.inlineStyleSheetsNumber>0) setRuleValues("externalizeCss",false,measures.inlineStyleSheetsNumber + " inline stylesheets found ");
     }
-
     measures.emptySrcTagNumber += frameMeasures.emptySrcTagNumber;
-    if (measures.emptySrcTagNumber>0) {
-      ecoRules.get("emptySrcTag").status = "NOK";
-      ecoRules.get("emptySrcTag").comment = measures.emptySrcTagNumber + " empty src tag(s) found";
-    }
-    
+    if (measures.emptySrcTagNumber>0) setRuleValues("emptySrcTag",false,measures.emptySrcTagNumber + " empty src tag(s) found");
+
     if (frameMeasures.inlineJsScript.length>0 ) analyseJsCode(frameMeasures.inlineJsScript,"inline");
    
     if (measures.inlineJsScriptsNumber < frameMeasures.inlineJsScriptsNumber) {
       measures.inlineJsScriptsNumber = frameMeasures.inlineJsScriptsNumber;
-      if (measures.inlineJsScriptsNumber > 0) {
-        if (measures.inlineJsScriptsNumber>1) ecoRules.get("externalizeJs").status = "NOK";
-        ecoRules.get("externalizeJs").comment = measures.inlineJsScriptsNumber + " inline  javascripts found ";
-      }
-      
+      if (measures.inlineJsScriptsNumber > 0) setRuleValues("externalizeJs",(measures.inlineJsScriptsNumber<2),measures.inlineJsScriptsNumber + " inline  javascripts found ");  
     }
-
-
+  }
+  
+  function setRuleValues(ruleId,isRespected,comment) {
+	if (isRespected)  ecoRules.get(ruleId).status = "OK";
+	else ecoRules.get(ruleId).status ="NOK"
+    ecoRules.get(ruleId).comment = comment;
   }
 
   function refreshUI() {
@@ -122,18 +97,14 @@ function   (esprima) {
     }
   }
 
-
   function launchAnalyse() {
-
     initializeMeasures();
     initializeEcoRules() 
- 
     // Launch analyse via injection of a script in each frame of the current tab
     backgroundPageConnection.postMessage({
       tabId: chrome.devtools.inspectedWindow.tabId,
       scriptToInject: "script/analyseFrame.js"
     });
-    
     getNetworkMeasure(); 
     getResourcesMeasure();
   }
@@ -146,7 +117,7 @@ function   (esprima) {
                "responsesSize":0,
                "responsesSizeUncompress":0,
                "ecoIndex":100,
-	       "grade":'A',
+	           "grade":'A',
                "pluginsNumber":0,
                "styleSheetsNumber":0,
                "printStyleSheetsNumber":0,
@@ -169,7 +140,7 @@ function   (esprima) {
     ecoRules = new Map();
     ecoRules.set("plugins",{ruleId:"plugins",status:"OK",comment:"No plugin found"});
     ecoRules.set("styleSheets",{ruleId:"styleSheets",status:"OK",comment:"Not more that 2 stylesheets per frame found"});
-    ecoRules.set("printStyleSheets",{ruleId:"printStyleSheets",status:"NOK",comment:"Not print stylesheet found"});
+    ecoRules.set("printStyleSheets",{ruleId:"printStyleSheets",status:"NOK",comment:"No print stylesheet found"});
     ecoRules.set("externalizeCss",{ruleId:"externalizeCss",status:"OK",comment:"No inline stylesheet found"});
     ecoRules.set("minifiedCss",{ruleId:"minifiedCss",status:"OK",comment:"No css found"});
     ecoRules.set("emptySrcTag",{ruleId:"emptySrcTag",status:"OK",comment:"No empty src tags found"});
@@ -182,8 +153,8 @@ function   (esprima) {
 
 
   function getNetworkMeasure() {
-  chrome.devtools.network.getHAR(function(result) {
-      var entries = result.entries;
+  chrome.devtools.network.getHAR(function(har) {
+      var entries = har.entries;
       var  domains = [];
       if (entries.length) {
         measures.nbRequest = entries.length;
@@ -197,14 +168,9 @@ function   (esprima) {
             debug(() => `found domain ${domain}`);
           }
         }
-        ecoRules.get("httpRequests").comment = measures.nbRequest + " HTTP request(s) "
-        if (measures.nbRequest > 26) ecoRules.get("httpRequests").status = "NOK";
-
+        setRuleValues("httpRequests",(measures.nbRequest < 27),measures.nbRequest + " HTTP request(s) ");
         measures.domainsNumber=domains.length;
-        ecoRules.get("domainsNumber").comment = domains.length + " domain(s) found";
-        if (measures.domainsNumber > 2) ecoRules.get("domainsNumber").status = "NOK";
-
-
+		setRuleValues("domainsNumber",(measures.domainsNumber < 3),domains.length + " domain(s) found"); 
         refreshUI();
           
       }
@@ -238,10 +204,7 @@ function analyseJsCode(code,url) {
     measures.jsErrorsNumber++;
     debug(() => `url ${url} : ${err} `);
   }
-  if (measures.jsErrorsNumber>0) {
-    ecoRules.get("jsValidate").status="NOK";
-    ecoRules.get("jsValidate").comment = measures.jsErrorsNumber + " javascript error(s) found";
-  }
+  if (measures.jsErrorsNumber>0)  setRuleValues("jsValidate",false,measures.jsErrorsNumber + " javascript error(s) found");
   refreshUI();
 }
 
@@ -273,9 +236,7 @@ function ResourceAnalyser(resource) {
     }
     else debug(() => `${url} is not minified`);
     measures.percentMinifiedJs = measures.minifiedJsNumber / measures.totalJs  * 100;
-    if (measures.percentMinifiedJs <95)  ecoRules.get("minifiedJs").status = "NOK";
-    else ecoRules.get("minifiedJs").status = "OK";
-    ecoRules.get("minifiedJs").comment = measures.percentMinifiedJs + " % ("+ measures.minifiedJsNumber+"/"+measures.totalJs +")minified javascript ";
+	setRuleValues("minifiedJs",(measures.percentMinifiedJs >= 95),measures.percentMinifiedJs + " % ("+ measures.minifiedJsNumber+"/"+measures.totalJs +") minified javascript "); 		
     refreshUI();
   }
 
@@ -288,9 +249,7 @@ function ResourceAnalyser(resource) {
     }
     else debug(() => `${url} is not minified`);
     measures.percentMinifiedCss = measures.minifiedCssNumber / measures.totalCss  * 100;
-    if (measures.percentMinifiedCss <95)  ecoRules.get("minifiedCss").status = "NOK";
-    else ecoRules.get("minifiedCss").status = "OK";
-    ecoRules.get("minifiedCss").comment = measures.percentMinifiedCss + " % ("+ measures.minifiedCssNumber+"/"+measures.totalCss +") minified stylesheet ";
+	setRuleValues("minifiedCss",(measures.percentMinifiedCss >= 95),measures.percentMinifiedCss + " % ("+ measures.minifiedCssNumber+"/"+measures.totalCss +") minified stylesheet "); 
     refreshUI();
   }
 
@@ -322,11 +281,11 @@ function ResourceAnalyser(resource) {
   function isMinified(scriptContent) {
 
     if (!scriptContent) return true;
-
+    if (scriptContent.length === 0) returntrue;
     const total = scriptContent.length-1;
     const semicolons = countChar(';', scriptContent);
     const linebreaks = countChar('\n', scriptContent);
-
+    if (linebreaks < 2) return true; 
     // Empiric method to detect minified files
     //
     // javascript code is minified if, on average:
