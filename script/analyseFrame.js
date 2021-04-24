6223afc2c85674035381f97e01751673bddc9654
didierfred <didierfred@gmail.com>
2021-04-24 17:57:42 +0200

    Deal with nested svg when computing dom size

--- script/analyseFrame.js
+++ script/analyseFrame.js
@@ -60,16 +60,18 @@ function getDomSizeWithoutSvg(){
   let dom_size = document.getElementsByTagName("*").length;
   const svgElements = document.getElementsByTagName("svg");
   for (let i = 0 ; i< svgElements.length ; i++) {
-    dom_size -= getNbChilds(svgElements[i])-1;
+    dom_size -= getNbChildsExcludingNestedSvg(svgElements[i])-1;
   }
   return dom_size;
 }
 
-function getNbChilds(element) {
+function getNbChildsExcludingNestedSvg(element) {
   if (element.nodeType === Node.TEXT_NODE) return 0;
   let nb_elements =1;
   for (let i = 0 ; i< element.childNodes.length ; i++) {
-    nb_elements+= getNbChilds(element.childNodes[i]);
+    // deal with svg nested case 
+    if (element.childNodes[i].tagName !== 'svg')  nb_elements+= getNbChildsExcludingNestedSvg(element.childNodes[i]);
+    else nb_elements+=1;
   }
   return nb_elements;
 }
