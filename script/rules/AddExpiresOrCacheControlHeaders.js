0f4bcd225adb6a494c4f42055f788c65d926374c
didierfred <didierfred@gmail.com>
2022-05-02 19:09:55 +0200

    Adapt rule expires or cache-control headers

--- script/rules/AddExpiresOrCacheControlHeaders.js
+++ script/rules/AddExpiresOrCacheControlHeaders.js
@@ -7,31 +7,28 @@ function createAddExpiresOrCacheControlHeadersRule() {
         comment: "",
         detailComment: "",
         specificMeasures: {
-            staticResourcesSize: 0,
+            staticResources: 0,
             staticResourcesWithCache: 0
         },
 
         check: function (measures) {
 
-            this.specificMeasures.staticResourcesSize =0;
+            this.specificMeasures.staticResources =0;
             this.specificMeasures.staticResourcesWithCache =0;
             if (measures.entries.length) measures.entries.forEach(entry => {
                 if (isStaticRessource(entry)) {
-                    this.specificMeasures.staticResourcesSize += entry.response.content.size;
+                    this.specificMeasures.staticResources += 1;
                     if (hasValidCacheHeaders(entry)) {
-                        this.specificMeasures.staticResourcesWithCache += entry.response.content.size;
-                    } else this.detailComment += chrome.i18n.getMessage("rule_AddExpiresOrCacheControlHeaders_DetailComment", `${entry.request.url} ${Math.round(entry.response.content.size / 100) / 10}`) + '<br>';
+                        this.specificMeasures.staticResourcesWithCache += 1;
+                    } else this.detailComment += chrome.i18n.getMessage("rule_AddExpiresOrCacheControlHeaders_DetailComment", entry.request.url) + '<br>';
                 }
             });
 
-            if (this.specificMeasures.staticResourcesSize > 0) {
-                const cacheHeaderRatio = this.specificMeasures.staticResourcesWithCache / this.specificMeasures.staticResourcesSize * 100;
-                if (cacheHeaderRatio < 95) {
-                    if (cacheHeaderRatio < 90) this.complianceLevel = 'C'
-                    else this.complianceLevel = 'B';
-                } else this.complianceLevel = 'A';
+            if (this.specificMeasures.staticResources > 0) {
+                if  (this.specificMeasures.staticResources - this.specificMeasures.staticResourcesWithCache > 0)  this.complianceLevel = 'C'
+                else this.complianceLevel = 'A';
                 this.comment = chrome.i18n.getMessage("rule_AddExpiresOrCacheControlHeaders_Comment",
-                    String(Math.round(cacheHeaderRatio * 10) / 10) + "%");
+                    this.specificMeasures.staticResourcesWithCache + ' / ' +   this.specificMeasures.staticResources);
             }
         },
 
