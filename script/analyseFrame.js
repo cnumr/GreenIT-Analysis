2d717d80358a1b9c36bb34234e6c3798a70dcbf9
didierfred <didierfred@gmail.com>
2022-11-01 15:04:54 +0100

    Adapt to be compatible with extension manifest V3

--- script/analyseFrame.js
+++ script/analyseFrame.js
@@ -1,6 +1,6 @@
 /*
  *  Copyright (C) 2016  The EcoMeter authors (https://gitlab.com/ecoconceptionweb/ecometer)
- *  Copyright (C) 2019  didierfred@gmail.com 
+ *  Copyright (C) 2019-2022  didierfred@gmail.com 
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
@@ -19,40 +19,13 @@
 function start_analyse() {
   const analyseStartingTime = Date.now();
   const dom_size = getDomSizeWithoutSvg();
-  let pageAnalysis;
-
-  if (analyseBestPractices) {
-    // test with http://www.wickham43.net/flashvideo.php
-    const pluginsNumber = getPluginsNumber();
-    const printStyleSheetsNumber = getPrintStyleSheetsNumber();
-    const inlineStyleSheetsNumber = getInlineStyleSheetsNumber();
-    const inlineJsScript = getInlineJsScript();
-    const inlineJsScriptsNumber = getInlineJsScriptsNumber();
-    const imagesResizedInBrowser = getImagesResizedInBrowser();
-
-
-    pageAnalysis = {
-      "analyseStartingTime": analyseStartingTime,
-      "url": document.URL,
-      "domSize": dom_size,
-      "pluginsNumber": pluginsNumber,
-      "printStyleSheetsNumber": printStyleSheetsNumber,
-      "inlineStyleSheetsNumber": inlineStyleSheetsNumber,
-      "inlineJsScript": inlineJsScript,
-      "inlineJsScriptsNumber": inlineJsScriptsNumber,
-      "imagesResizedInBrowser": imagesResizedInBrowser,
-    }
-  }
-  else pageAnalysis = {
+  const pageAnalysis = {
     "analyseStartingTime": analyseStartingTime,
     "url": document.URL,
     "domSize": dom_size
   }
-
   chrome.runtime.sendMessage(pageAnalysis);
-
 }
-
 
 function getDomSizeWithoutSvg(){
   let dom_size = document.getElementsByTagName("*").length;
@@ -72,80 +45,6 @@ function getNbChildsExcludingNestedSvg(element) {
     else nb_elements+=1;
   }
   return nb_elements;
-}
-
-
-function getPluginsNumber() {
-  const plugins = document.querySelectorAll('object,embed');
-  return (plugins === undefined) ? 0 : plugins.length;
-}
-
-
-
-function getPrintStyleSheetsNumber() {
-  return document.querySelectorAll('link[rel=stylesheet][media~=print]').length
-    + document.querySelectorAll('style[media~=print]').length;
-}
-
-function getInlineStyleSheetsNumber() {
-  let styleSheetsArray = Array.from(document.styleSheets);
-  let inlineStyleSheetsNumber = 0;
-  styleSheetsArray.forEach(styleSheet => {
-    try {
-      // Ignore SVG styles in count 
-      const isSvgStyleSheet = (styleSheet.ownerNode instanceof SVGStyleElement);
-      if (!styleSheet.href && !isSvgStyleSheet) inlineStyleSheetsNumber++;
-    }
-    catch (err) {
-      console.log("GREENIT-ANALYSIS ERROR ," + err.name + " = " + err.message);
-      console.log("GREENIT-ANALYSIS ERROR " + err.stack);
-    }  
-  });
-return inlineStyleSheetsNumber;
-}
-
-
-function getInlineJsScript() {
-  let scriptArray = Array.from(document.scripts);
-  let scriptText = "";
-  scriptArray.forEach(script => {
-    let isJSON = (String(script.type) === "application/ld+json"); // Exclude type="application/ld+json" from parsing js analyse
-    if ((script.text.length > 0) && (!isJSON)) scriptText += "\n" + script.text;
-  });
-  return scriptText;
-}
-
-function getInlineJsScriptsNumber() {
-  let scriptArray = Array.from(document.scripts);
-  let inlineScriptNumber = 0;
-  scriptArray.forEach(script => {
-    let isJSON = (String(script.type) === "application/ld+json"); // Exclude type="application/ld+json" from count
-    if ((script.text.length > 0) && (!isJSON)) inlineScriptNumber++;
-  });
-  return inlineScriptNumber;
-}
-
-
-function getImagesResizedInBrowser() {
-  const imgArray = Array.from(document.querySelectorAll('img'));
-  let imagesResized = [];
-  imgArray.forEach(img => {
-    if (img.clientWidth < img.naturalWidth || img.clientHeight < img.naturalHeight) {
-      // Images of one pixel are some times used ... , we exclude them
-      if (img.naturalWidth > 1) 
-      {
-        const imageMeasures = {
-          "src":img.src,
-          "clientWidth":img.clientWidth,
-          "clientHeight":img.clientHeight,
-          "naturalWidth":img.naturalWidth,
-          "naturalHeight":img.naturalHeight
-        }
-        imagesResized.push(imageMeasures);
-      }
-    }
-  });
-  return imagesResized;
 }
 
 
