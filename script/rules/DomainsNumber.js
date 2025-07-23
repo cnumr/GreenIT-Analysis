130aec3ec7157d2520966c3b61e1301a032b7897
didierfred <didierfred@gmail.com>
2025-07-23 19:32:41 +0200

    Mise à jour dans le cadre des régles V5

--- script/rules/DomainsNumber.js
+++ script/rules/DomainsNumber.js
@@ -1,36 +1,38 @@
 rulesManager.registerRule(createDomainsNumberRule(), "harReceived");
 
 function createDomainsNumberRule() {
-    return {
-        complianceLevel: 'A',
-        id: "DomainsNumber",
-        comment: "",
-        detailComment: "",
-        specificMeasures: {
-            domains: []
-        },
+  return {
+    complianceLevel: "A",
+    id: "DomainsNumber",
+    comment: "",
+    detailComment: "",
+    specificMeasures: {
+      domains: [],
+    },
 
-        check: function (measures) {
-            this.specificMeasures.domains = [];
-            if (measures.entries.length) measures.entries.forEach(entry => {
-                let domain = getDomainFromUrl(entry.request.url);
-                if (this.specificMeasures.domains.indexOf(domain) === -1) {
-                    this.specificMeasures.domains.push(domain);
-                }
-            });
-            if (this.specificMeasures.domains.length > 3) {
-                if (this.specificMeasures.domains.length < 6) this.complianceLevel = 'B';
-                else this.complianceLevel = 'C';
-            }
-            this.specificMeasures.domains.forEach(domain => {
-                this.detailComment += domain + "<br>";
-            });
+    check: function (measures) {
+      this.specificMeasures.domains = [];
+      if (measures.entries.length)
+        measures.entries.forEach((entry) => {
+          let domain = getDomainFromUrl(entry.request.url);
+          if (this.specificMeasures.domains.indexOf(domain) === -1) {
+            this.specificMeasures.domains.push(domain);
+          }
+        });
+      if (this.specificMeasures.domains.length > 5) this.complianceLevel = "C";
+      this.specificMeasures.domains.forEach((domain) => {
+        this.detailComment += domain + "<br>";
+      });
 
-            this.comment = chrome.i18n.getMessage("rule_DomainsNumber_Comment", String(this.specificMeasures.domains.length));
-        },
+      this.comment = chrome.i18n.getMessage(
+        "rule_DomainsNumber_Comment",
+        String(this.specificMeasures.domains.length),
+      );
+    },
 
-        getSpecificMeasures: function () {
-            return this.specificMeasures;
-        }
-    }
+    getSpecificMeasures: function () {
+      return this.specificMeasures;
+    },
+  };
 }
+
